5b08805857c5206a1baf00a0a1bbb06e1e6ecf1f
seyisu <128826598+seyisu@users.noreply.github.com>
2023-09-05 13:23:21 +0200

    Change camera inversion in Inspector.
    - Remove sensitivity parameter as it doesn't work

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,9 +9,6 @@ public class CameraController : MonoBehaviour, AxisState.IInputAxisProvider
     [Header("Input Actions")]
     [SerializeField] [Required] private InputActionAsset actionAsset;
 
-    [Header("Camera Parameters")]
-    [SerializeField] [Range(0.01f, 2.00f)] private float sensitivity = 0.5f;
-
     private InputAction lookAction;
     private Vector2 lookInput;
 
@@ -50,6 +47,6 @@ public class CameraController : MonoBehaviour, AxisState.IInputAxisProvider
 
     private void OnLook(InputAction.CallbackContext context)
     {
-        lookInput = new Vector2(-context.ReadValue<Vector2>().x, -context.ReadValue<Vector2>().y) * sensitivity;
+        lookInput = new Vector2(context.ReadValue<Vector2>().x, context.ReadValue<Vector2>().y);
     }
 }
